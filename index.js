3850d4d89987c5479322ab824f4c6747768b4a6d
Adam Keresztes <akeresztes@ptc.com>
2022-12-16 14:20:32 +0100

    simple api demonstration

--- index.js
+++ index.js
@@ -9,3 +9,39 @@ api.authenticate()
     }))
     .then(response => response.json())
     .then(projects => console.log(projects));
+
+api.onSelectedItemChange((itemId) => {
+    console.log("onSelectedItemChange");
+    console.log(itemId);
+    let messagesDiv = document.getElementById('messages');
+
+    messagesDiv.innerHTML += "<br>onSelectedItemChange: <br>";
+    messagesDiv.innerHTML += itemId.toString();
+    post(itemId);
+});
+
+document.addEventListener("DOMContentLoaded",function(){
+    api.getSelectedItem().then(selectedItem => {
+        console.log("selectedItemId");
+        console.log(selectedItem);
+        let messages = document.getElementById('messages');
+        messages.innerHTML += "<br>selectedItemId: " + selectedItem.itemId;
+        post(selectedItem.itemId);
+    });
+});
+
+const post = (itemId) => {
+    (async () => {
+        const rawResponse = await fetch('http://localhost:3080/api/example', {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({itemId: itemId})
+        });
+        const content = await rawResponse.json();
+
+        console.log(content);
+    })();
+}
